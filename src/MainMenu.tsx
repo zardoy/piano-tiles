bc51b3582e3d5185c454863eb40ae8acded392db
Vitaly Turovsky <vital2580@icloud.com>
2021-03-25 18:56:21 +0300

    feat: add ability to customize grid

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Button, Grid, Slider, Typography } from "@material-ui/core";
+
+import { gameConfig } from "./Game";
 
 interface GameMode {
     label: string;
@@ -19,21 +21,44 @@ const BigText: React.FC<React.ComponentProps<typeof Typography>> = (props) => {
 };
 
 const MainMenu: React.FC = () => {
-    return <Grid container direction="column" justify="flex-start" alignContent="center" style={{ padding: 50 }}>
-        <BigText variant="h3">Piano Tiles online</BigText>
-        <BigText variant="h5" color="textSecondary">Select Game Mode</BigText>
-        {
-            gameModes.map(({ id: modeId, label }) =>
-                <Button
-                    key={modeId}
-                    component={Link}
-                    to={`/game/${modeId}`}
-                    variant="outlined"
-                    size="large"
-                >{label}</Button>
-            )
-        }
-    </Grid>;
+    return <>
+        <Grid container direction="column" justify="flex-start" alignContent="center" style={{ padding: 50 }}>
+            <BigText variant="h3">Piano Tiles online</BigText>
+            <BigText variant="h5" color="textSecondary">Select Game Mode</BigText>
+            {
+                gameModes.map(({ id: modeId, label }) =>
+                    <Button
+                        key={modeId}
+                        component={Link}
+                        to={`/game/${modeId}`}
+                        variant="outlined"
+                        size="large"
+                    >{label}</Button>
+                )
+            }
+            <BigText variant="h5" color="textSecondary">Game Grid Configuration</BigText>
+            <Grid container style={{ width: "auto" }}>
+                {
+                    (["width", "height"] as (keyof typeof gameConfig.tilesQuantity)[]).map(prop => {
+                        const min = prop === "height" ? 2 : 1;
+                        return <>
+                            <Typography>{prop}</Typography>
+                            <Slider
+                                key={prop}
+                                defaultValue={gameConfig.tilesQuantity[prop]}
+                                valueLabelDisplay="auto"
+                                step={1}
+                                min={min}
+                                max={8}
+                                onChange={(_, val) => gameConfig.tilesQuantity[prop] = val as number}
+                                marks
+                            />
+                        </>;
+                    })
+                }
+            </Grid>
+        </Grid>
+    </>;
 };
 
 export default MainMenu;
